e1dfe4e9ac50319d69584732c0d60f19b64d11c0
Ridho Pratama <p.ridho@yahoo.co.id>
2021-12-01 18:44:47 +0700

    Cache day input

--- lib/aoc/util.ex
+++ lib/aoc/util.ex
@@ -1,12 +1,28 @@
 defmodule AOC.Util do
   def load(day) do
-    url = "https://adventofcode.com/2021/day/#{day}/input"
-    response = HTTPoison.get!(url, [{"Cookie", Application.get_env(:aoc, :cookie)}])
+    {:ok, dets} = :dets.open_file(:"storage.dets", [{:type, :set}])
 
-    response.body
-    |> String.trim()
-    |> String.split("\n")
-    |> Enum.filter(&(&1 != ""))
+    res =
+      case :dets.lookup(dets, day) do
+        [] ->
+          url = "https://adventofcode.com/2021/day/#{day}/input"
+          response = HTTPoison.get!(url, [{"Cookie", Application.get_env(:aoc, :cookie)}])
+
+          result =
+            response.body
+            |> String.trim()
+            |> String.split("\n")
+            |> Enum.filter(&(&1 != ""))
+
+          :dets.insert(dets, {day, result})
+          result
+
+        [{_, res}] ->
+          res
+      end
+
+    :dets.close(dets)
+    res
   end
 
   def load_integers(day) do
